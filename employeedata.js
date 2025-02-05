eb0b7ac893eba9b8f623729f84e3419c71a90601
Pranjal Maithani <Pranjalmaithani81822@gmail.com>
2025-02-05 16:49:57 +0530

    Add files via upload

---
+++ employeedata.js
@@ -0,0 +1,9 @@
+const employeeData = [
+    { 
+        name: "Pranjal Maithani",
+        mobno: "8791273578",
+        emailid: "pranjalmaithani81822@gmail.com",
+        role: "admin"
+    },
+    // Add more employees as needed
+];
