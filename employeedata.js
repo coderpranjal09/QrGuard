110b97fb543d85b0df9f04974e547bfeeeb7048a
Pranjal Maithani <Pranjalmaithani81822@gmail.com>
2025-02-18 17:41:41 +0530

    Update employeedata.js

--- employeedata.js
+++ employeedata.js
@@ -4,6 +4,11 @@ const employeeData = [
         mobno: "8791273578",
         emailid: "pranjalmaithani81822@gmail.com",
         role: "admin"
-    },
+    },{
+        name:"Santosh Bohra",
+        mobno:"8126376009",
+        emailid:"universebhaijan111@gmail.com",
+        role:"work office"
+    }
     // Add more employees as needed
 ];
